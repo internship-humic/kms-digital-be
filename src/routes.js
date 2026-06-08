34ae5ebdac3d5d82d70f7f2cf124a4d4993767f3
admltfr <adamlutfiramadhan@gmail.com>
2026-06-08 17:50:11 +0700

    fix(region): forgot to add endpoint prefix

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,9 @@
 import authRoutes from "./domains/auth/auth.routes.js";
+import regionRoutes from "./domains/region/region.routes.js";
 
 const routes = [
   { path: "/auth", route: authRoutes },
+  { path: "/region", route: regionRoutes },
 ];
 
 export default routes;
