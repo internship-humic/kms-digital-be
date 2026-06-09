4229c88c3050d807f9ff56b254b02fa70dadb408
admltfr <adamlutfiramadhan@gmail.com>
2026-06-09 12:38:55 +0700

    feat(make:domain): add a mechanism that can add route prefix automatically

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,6 @@
 import authRoutes from "./domains/auth/auth.routes.js";
 import regionRoutes from "./domains/region/region.routes.js";
 import clinicRoutes from "./domains/clinic/clinic.routes.js";
-
 const routes = [
   { path: "/auth", route: authRoutes },
   { path: "/region", route: regionRoutes },
--- src/utils/make-domain.util.js
+++ src/utils/make-domain.util.js
@@ -9,7 +9,7 @@ const ROOT = process.cwd();
 program
   .command("make:domain <name>")
   .description(
-    "Generate a new domain folder with controller, service, routes, and schema"
+    "Generate a new domain folder with controller, service, routes, and schema",
   )
   .action((name) => {
     const domainParts = name.split("/");
@@ -21,29 +21,30 @@ program
     }
 
     fs.ensureDirSync(domainPath);
-    name = domainParts.at(-1);
 
-    const className = (name) => {
-      return name
+    const domainName = domainParts.at(-1);
+
+    const className = (value) => {
+      return value
         .split(/[-_ ]+/)
         .map((s) => capitalize(s))
         .join("");
     };
 
-    const schemaName = (name) => {
-      return name.split(/[-_ ]+/).join("");
+    const schemaName = (value) => {
+      return value.split(/[-_ ]+/).join("");
     };
 
     const controllerTemplate = `
-import ${className(name)}Service from "./${name}.service.js";
+import ${className(domainName)}Service from "./${domainName}.service.js";
 import BaseController from "../../common/base_classes/base-controller.js";
 
-class ${className(name)}Controller extends BaseController {
+class ${className(domainName)}Controller extends BaseController {
   constructor() {
-    super(${className(name)}Service);
+    super(${className(domainName)}Service);
     // this.error = BaseError
     // this.response = BaseResponse
-    // this.service = ${className(name)}Service
+    // this.service = ${className(domainName)}Service
   }
 
   async someMethod(req, res) {
@@ -51,88 +52,124 @@ class ${className(name)}Controller extends BaseController {
   }
 }
 
-export default new ${className(name)}Controller();
+export default new ${className(domainName)}Controller();
 `;
 
     const serviceTemplate = `
 import BaseService from "../../common/base_classes/base-service.js";
 
-class ${className(name)}Service extends BaseService {
+class ${className(domainName)}Service extends BaseService {
   constructor() {
     super();
     // this.error = BaseError
     // this.db = Prisma
   }
-  
+
   async someMethod() {
     // implement method logic here
   }
 }
 
-export default new ${className(name)}Service();
+export default new ${className(domainName)}Service();
 `;
 
     const routesTemplate = `
-import ${className(name)}Controller from "./${name}.controller.js";
+import ${className(domainName)}Controller from "./${domainName}.controller.js";
 import BaseRoutes from "../../common/base_classes/base-routes.js";
-import { ${schemaName(name)}Schema } from './${name}.schema.js';
+import { ${schemaName(domainName)}Schema } from "./${domainName}.schema.js";
 
-class ${className(name)}Routes extends BaseRoutes {
+class ${className(domainName)}Routes extends BaseRoutes {
   constructor() {
-    super(${className(name)}Controller);
-    //this.router = Router();
-    //this.auth = AuthMiddleware;
-    //this.validate = Validate;
-    //this.errCatch = ErrorMiddleware.errorCatcher;
-    //this.controller = controller;
-    //this.roles = Roles;
-    //this.routes();
+    super(${className(domainName)}Controller);
+    // this.router = Router();
+    // this.auth = AuthMiddleware;
+    // this.validate = Validate;
+    // this.errCatch = ErrorMiddleware.errorCatcher;
+    // this.controller = controller;
+    // this.roles = Roles;
+    // this.routes();
   }
 
   routes() {
     this.router.get("/:id", [
       this.auth.authenticate,
-      this.auth.role([this.roles.Farmer]),
-      this.errCatch(this.controller.someMethod.bind(this.controller))
+      this.auth.role([this.roles.Admin]),
+      this.errCatch(this.controller.someMethod.bind(this.controller)),
     ]);
+
     this.router.post("/", [
       this.auth.authenticate,
-      this.auth.role([this.roles.Farmer]),
-      this.validate(${schemaName(name)}Schema),
-      this.errCatch(this.controller.someMethod.bind(this.controller))
+      this.auth.role([this.roles.Admin]),
+      this.validate(${schemaName(domainName)}Schema),
+      this.errCatch(this.controller.someMethod.bind(this.controller)),
     ]);
   }
 }
 
-export default new ${className(name)}Routes().router;
+export default new ${className(domainName)}Routes().router;
 `;
 
     const schemaTemplate = `
 import Joi from "joi";
 
-const ${schemaName(name)}Schema = Joi.object({
+const ${schemaName(domainName)}Schema = Joi.object({
   // Define your validation schema here
 });
 
-export { ${schemaName(name)}Schema };
+export { ${schemaName(domainName)}Schema };
 `;
 
     fs.writeFileSync(
-      path.join(domainPath, `${name}.controller.js`),
-      controllerTemplate
+      path.join(domainPath, `${domainName}.controller.js`),
+      controllerTemplate,
     );
+
     fs.writeFileSync(
-      path.join(domainPath, `${name}.service.js`),
-      serviceTemplate
+      path.join(domainPath, `${domainName}.service.js`),
+      serviceTemplate,
     );
+
     fs.writeFileSync(
-      path.join(domainPath, `${name}.routes.js`),
-      routesTemplate
+      path.join(domainPath, `${domainName}.routes.js`),
+      routesTemplate,
     );
+
     fs.writeFileSync(
-      path.join(domainPath, `${name}.schema.js`),
-      schemaTemplate
+      path.join(domainPath, `${domainName}.schema.js`),
+      schemaTemplate,
     );
+
+    const routesFile = path.join(ROOT, "src", "routes.js");
+
+    if (fs.existsSync(routesFile)) {
+      let routesContent = fs.readFileSync(routesFile, "utf8");
+
+      const importPath = `./domains/${domainParts.join("/")}/${domainName}.routes.js`;
+
+      const importLine = `import ${domainName}Routes from "${importPath}";`;
+
+      const routeLine = `  { path: "/${domainName}", route: ${domainName}Routes },`;
+
+      if (!routesContent.includes(importLine)) {
+        const exportIndex = routesContent.indexOf("const routes =");
+
+        if (exportIndex !== -1) {
+          routesContent =
+            routesContent.slice(0, exportIndex) +
+            `${importLine}\n` +
+            routesContent.slice(exportIndex);
+        }
+      }
+
+      if (!routesContent.includes(`route: ${domainName}Routes`)) {
+        routesContent = routesContent.replace(
+          /const routes = \[/,
+          `const routes = [\n${routeLine}`,
+        );
+      }
+
+      fs.writeFileSync(routesFile, routesContent);
+    }
 
     logger.info(`Domain '${name}' created successfully!`);
   });
