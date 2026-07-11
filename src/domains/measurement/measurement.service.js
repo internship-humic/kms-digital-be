3d99c86ad076a0fec0899096c998b2f88be3a146
admltfr <adamlutfiramadhan@gmail.com>
2026-07-11 18:45:43 +0700

    fix(measurement): fix missing import issue

--- src/domains/measurement/measurement.service.js
+++ src/domains/measurement/measurement.service.js
@@ -1,6 +1,7 @@
 import BaseService from "../../common/base_classes/base-service.js";
 
 import calculateAgeInMonths from "../../utils/age.util.js";
+import calculateRiskStatus from "../../utils/risk-status.util.js";
 import { calculateAllZScores } from "../../utils/zscore.util.js";
 import { getPagination, getMeta } from "../../utils/pagination.util.js";
 import { ORMfilterable } from "../../utils/query.util.js";
